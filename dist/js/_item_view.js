150b50857a7aebc697b09fbeebea653ccdad140a
jini9898 <iammes98@gmail.com>
2023-07-29 01:44:11 +0900

    cart-layout

--- dist/js/_item_view.js
+++ dist/js/_item_view.js
@@ -8,22 +8,24 @@ var swiper = new Swiper(".swiper-container-mySwiper", {
 });
 
 var modal = document.querySelector(".modal");
-var closeButtons = document.querySelectorAll(".close-modal");
-// set open modal behaviour
-document.querySelector(".cart-button").addEventListener("click", function () {
-  modal.classList.toggle("modal-dimmed");
+var openButton = document.querySelector(".cart-go-button");
+var closeButton = document.querySelector(".close-modal");
+var modalWrap = document.querySelector(".modal-wrap");
+
+// cart-go-button을 클릭하면 모달이 나타나도록 이벤트 리스너를 추가합니다.
+openButton.addEventListener("click", function () {
+  modal.classList.add("modal-dimmed");
 });
-// set close modal behaviour
-for (i = 0; i < closeButtons.length; ++i) {
-  closeButtons[i].addEventListener("click", function () {
-    modal.classList.toggle("modal-dimmed");
-  });
-}
-// close modal if clicked outside content area
-document.querySelector(".modal-wrap").addEventListener("click", function () {
-  modal.classList.toggle("modal-dimmed");
+
+// x 버튼을 클릭하면 모달이 닫히도록 이벤트 리스너를 추가합니다.
+closeButton.addEventListener("click", function () {
+  modal.classList.remove("modal-dimmed");
 });
-// prevent modal inner from closing parent when clicked
-document.querySelector(".modal-dimmed").addEventListener("click", function (e) {
-  e.stopPropagation();
+
+// 모달 바깥 부분을 클릭하면 모달이 닫히도록 이벤트 리스너를 추가합니다.
+modalWrap.addEventListener("click", function (e) {
+  // 클릭이 모달 내부에서 발생했는지를 확인합니다.
+  if (e.target.classList.contains("modal-wrap")) {
+    modal.classList.remove("modal-dimmed");
+  }
 });
