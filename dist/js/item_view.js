fc1f339dfc42c88b85b4a5a688a341786a88f71e
jini9898 <iammes98@gmail.com>
2023-08-01 18:16:49 +0900

    class 이름 변경

--- dist/js/item_view.js
+++ dist/js/item_view.js
@@ -7,25 +7,25 @@ var swiper = new Swiper(".swiper-container-mySwiper", {
   },
 });
 
-var modal = document.querySelector(".modal");
+var modal = document.querySelector("#modal");
 var openButton = document.querySelector(".cart-go-button");
 var closeButton = document.querySelector(".close-modal");
-var modalWrap = document.querySelector(".modal-wrap");
+var modalWrap = document.querySelector(".modal-container");
 
 // cart-go-button을 클릭하면 모달이 나타나도록 이벤트 리스너를 추가합니다.
 openButton.addEventListener("click", function () {
-  modal.classList.add("modal-dimmed");
+  modal.classList.add("open");
 });
 
 // x 버튼을 클릭하면 모달이 닫히도록 이벤트 리스너를 추가합니다.
 closeButton.addEventListener("click", function () {
-  modal.classList.remove("modal-dimmed");
+  modal.classList.remove("open");
 });
 
 // 모달 바깥 부분을 클릭하면 모달이 닫히도록 이벤트 리스너를 추가합니다.
 modalWrap.addEventListener("click", function (e) {
   // 클릭이 모달 내부에서 발생했는지를 확인합니다.
-  if (e.target.classList.contains("modal-wrap")) {
-    modal.classList.remove("modal-dimmed");
+  if (e.target.classList.contains("modal-container")) {
+    modal.classList.remove("open");
   }
 });
